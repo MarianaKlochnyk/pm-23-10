b404ec52acbf7ce5f8a771403f3acb1a10a56194
MarianaKlochnyk <mariana.klochnyk.pm.2023@lpnu.ua>
2024-09-17 11:28:07 +0300

    Enit file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const htmlmin = require('gulp-htmlmin');
+const fileinclude = require('gulp-file-include');
+const imagemin=require('gulp-imagemin')
 
 function buildStyles() {
   return gulp.src('./app/scss/**/*.scss')
@@ -13,3 +16,25 @@ exports.buildStyles = buildStyles;
 exports.watch = function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 };
+
+gulp.task('minify', () => {
+  return gulp.src('app/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fileinclude', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+exports.html = gulp.series('fileinclude', 'minify')
+
+ function images(){
+  return gulp.src('app/img/**/*.png').pipe(imagemin()).pipe(gulp.dest('dist/img'))
+ }
+ exports.images = images
